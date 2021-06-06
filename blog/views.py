e84deaa1dbcab4c57563891618376a4647e50e1b
alankar23 <alankarnimgade@gmail.com>
2021-06-06 11:33:32 +0530

    second comit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def starting_page(requests):
+    return render(requests, 'blog/index.html')
+
+
+def posts(requests):
+    pass
+
+
+def post_detail(requests):
+    pass
